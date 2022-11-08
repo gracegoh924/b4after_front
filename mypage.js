8f230577eb8b66858b009bf1e9b20e3a1835bf3a
gracegoh <gracegoh924@gmail.com>
2022-11-08 11:01:06 +0900

    url 오타 수정

--- mypage.js
+++ mypage.js
@@ -5,16 +5,10 @@ function getUser() {
     fetch("https://jsonplaceholder.typicode.com/users/1", config)
       .then(response => response.json())
       .then(data => {
-        const name = document.createElement("div");
         const email = document.createElement("div");
-        const phone = document.createElement("div");
-        name.textContent = data.name;
         email.textContent = data.email;
-        phone.textContent = data.phone;
         const userInfo = document.getElementById("userInfo");
-        userInfo.appendChild(name);
         userInfo.appendChild(email);
-        userInfo.appendChild(phone);
       })
       .catch(error => console.log("fetch 에러!"));
   }
