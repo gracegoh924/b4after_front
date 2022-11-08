62fbd78a46c22620bec64181d8d6647ae0fc288e
gracegoh <gracegoh924@gmail.com>
2022-11-08 11:22:09 +0900

    first commit

--- home.js
+++ home.js
@@ -9,15 +9,20 @@ async function loadhome(){
     console.log(images)
     console.log(images.top_ten)
     console.log(images.rec_books)
-    const img = document.createElement('img')
 
-    img.setAttribute("src", "http://127.0.0.1:8000"+images.top_ten)
-    img.setAttribute("src", "http://127.0.0.1:8000"+images.rec_books)
+    const img1 = document.createElement('img')
+    const img2 = document.createElement('img')
+
+    img1.setAttribute("src", "http://127.0.0.1:8000"+images.top_ten)
+    homes.appendChild(img1)
+
+    img2.setAttribute("src", "http://127.0.0.1:8000"+images.rec_books)
 
 
-    img.classList.add('card-img-top')
+    img1.classList.add('card-img-top')
+    img2.classList.add('card-img-top')
 
-    homes.appendChild(img)
+    homes.appendChild(img2)
 }
 
 loadhome();
